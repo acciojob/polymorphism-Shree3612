5f6f805e762ea550b2c5878224aba0c65589c7d3
Shree3612 <160580049+Shree3612@users.noreply.github.com>
2024-10-17 20:56:33 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-
+   public class Product {
+      Product  p = new Product();
+      p.product(1,2);
+      p.product(1,2,3);
+      p.product(1.0,2.0);
+      public int product(int x, int y) {
+        
+      }
+     public int product(int x, int y, int z) {
+       
+     }
+     public double product(double x, double y) {
+       
+     }
+   }
 }
