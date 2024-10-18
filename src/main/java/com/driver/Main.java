c8546c13da2388c692c251469baa2e0a4f6d0985
Shree3612 <160580049+Shree3612@users.noreply.github.com>
2024-10-18 17:17:11 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,13 +7,13 @@ public class Main {
       p.product(1,2,3);
       p.product(1.0,2.0);
       public int product(int x, int y) {
-        
+        return x*y;
       }
      public int product(int x, int y, int z) {
-       
+       return x*y*z;
      }
      public double product(double x, double y) {
-       
+       return x*y;
      }
    }
 }
